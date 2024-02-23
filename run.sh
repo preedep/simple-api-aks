09d793ca58cb336cc61f7be2bef4ca50e61d9b85
Preedee Ponchevin <nickmsft@mac.com>
2024-02-23 22:01:02 +0700

    Add run local

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 docker stop aks-simple-api
 docker rm aks-simple-api
 
-docker run -p 8888:8888 --name aks-simple-api -d nickmsft/simple-api-aks:latest
+docker run --env-file ./my_env -p 8888:8888 --name aks-simple-api -d nickmsft/simple-api-aks:latest
